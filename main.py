d5af68cf132512e91a65b76b1925ba58eda428fe
DarkLe3der <amnb9@walla.com>
2020-10-07 23:54:26 +0300

    failed attempt to add stockfish

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import pygame
 from chess.constants import WIDTH, HEIGHT, FPS, BOARD_EDGE
 from chess.game import Game
+from chess.engine import Engine
 pygame.init()
 window = pygame.display.set_mode((WIDTH + BOARD_EDGE * 2, HEIGHT + BOARD_EDGE * 2))
 pygame.display.set_caption("Chess")
 clock = pygame.time.Clock()
 game = Game(window)
+engine = Engine()
+engine.get_response()
+engine.put_command("uci")
+engine.get_response()
+engine.put_command('quit')
 
 running = True
 while running:
